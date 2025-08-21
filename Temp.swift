a141b704baf7941d6d6e753b3b44f6e0a515edd2
jhyoo93 <167679028+jhyoo93@users.noreply.github.com>
2025-08-21 23:06:16 +0900

    함수

--- Temp.swift
+++ Temp.swift
@@ -1,7 +1,15 @@
 //
 //  Temp.swift
-//  
-//
 //  Created by 유재환 on 8/21/25.
 //
+//  자유로운 코드 연습 공간~
+//
 
+import Foundation
+
+func myFunction(name: String) -> String {
+    return "안녕하세요! \(name) 입니다!"
+}
+
+// 함수, 메소드를 호출한다.
+myFunction(name: "유재환")
